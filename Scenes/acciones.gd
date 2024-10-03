d55b912fab53ff76808281f22a3463caaedf6450
Federico Puente <30831720+Hashiyama89@users.noreply.github.com>
2024-10-02 22:29:48 -0400

    Cambio las constantes

--- Scenes/acciones.gd
+++ Scenes/acciones.gd
@@ -1,16 +1,16 @@
 extends Panel
 
 func _on_shuffle_doors_pressed():
-	NetworkManager.shuffle_deck(Game.DECK_TYPE.DRAW_DOOR)
+	NetworkManager.shuffle_deck(Util.DECK_TYPE.DRAW_DOOR)
 
 
 func _on_shuffle_treasures_pressed():
-	NetworkManager.shuffle_deck(Game.DECK_TYPE.DRAW_TREASURE)
+	NetworkManager.shuffle_deck(Util.DECK_TYPE.DRAW_TREASURE)
 
 
 func _on_shuffle_discard_doors_pressed():
-	NetworkManager.shuffle_deck(Game.DECK_TYPE.DISCARD_DOOR)
+	NetworkManager.shuffle_deck(Util.DECK_TYPE.DISCARD_DOOR)
 
 
 func _on_shuffle_discard_treasures_pressed():
-	NetworkManager.shuffle_deck(Game.DECK_TYPE.DISCARD_TREASURE)
+	NetworkManager.shuffle_deck(Util.DECK_TYPE.DISCARD_TREASURE)
