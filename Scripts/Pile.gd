0115b842b51c7cf7eeee89d2fe7c7bfce48d9b17
Federico Puente <30831720+Hashiyama89@users.noreply.github.com>
2024-10-03 23:49:56 -0400

    Añade mezclar cartas en todos los decks, incluso los de descarte

--- Scripts/Pile.gd
+++ Scripts/Pile.gd
@@ -2,13 +2,48 @@ class_name Pile
 extends Container
 
 static var piles: Array[Pile] = []
-@export var pile_name: String
+@export var pileType: Util.PILE_TYPE
 
 func _ready():
 	piles.append(self)
 
 
-static func findPile(pileName: String):
+static func findPile(pileType: Util.PILE_TYPE):
 	for pile in piles:
-		if pile.pile_name == pileName:
+		if pile.pileType == pileType:
 			return pile
+
+
+func shuffle():
+	if self.pileType == Util.PILE_TYPE.DOOR || self.pileType == Util.PILE_TYPE.TREASURE:
+		var cardsInPile: Array[int] = []
+		for child in get_children():
+			cardsInPile.append(child.cardNumber)
+		
+		cardsInPile.shuffle()
+		cardsInPile.shuffle()
+		cardsInPile.shuffle()
+		updatePile(cardsInPile)
+			
+		return cardsInPile
+
+func updatePile(newOrder: Array[int]):
+	var cardType = _getTypeCard()
+	for i in range(newOrder.size()):
+		var card = Card.findCard(newOrder[i], cardType)
+		self.move_child(card, i)
+
+
+func _getTypeCard():
+	if self.pileType == Util.PILE_TYPE.DOOR || self.pileType == Util.PILE_TYPE.DISCARD_DOOR:
+		return Util.CARD_TYPE.DOOR
+	elif self.pileType == Util.PILE_TYPE.TREASURE || self.pileType == Util.PILE_TYPE.DISCARD_TREASURE:
+		return Util.CARD_TYPE.TREASURE
+	
+	return null	
+	
+func send_cards_to(pileType: Util.PILE_TYPE):
+	var newPile = findPile(pileType)
+	for child in get_children():
+		child.isFaceDown = true
+		child.reparent(newPile)
--- Scripts/acciones.gd
+++ Scripts/acciones.gd
@@ -1,16 +1,16 @@
 extends Panel
 
 func _on_shuffle_doors_pressed():
-	NetworkManager.shuffle_deck(Util.DECK_TYPE.DRAW_DOOR)
+	NetworkManager.shuffle_deck(Util.PILE_TYPE.DOOR)
 
 
 func _on_shuffle_treasures_pressed():
-	NetworkManager.shuffle_deck(Util.DECK_TYPE.DRAW_TREASURE)
+	NetworkManager.shuffle_deck(Util.PILE_TYPE.TREASURE)
 
 
 func _on_shuffle_discard_doors_pressed():
-	NetworkManager.shuffle_deck(Util.DECK_TYPE.DISCARD_DOOR)
+	NetworkManager.shuffle_deck(Util.PILE_TYPE.DISCARD_DOOR)
 
 
 func _on_shuffle_discard_treasures_pressed():
-	NetworkManager.shuffle_deck(Util.DECK_TYPE.DISCARD_TREASURE)
+	NetworkManager.shuffle_deck(Util.PILE_TYPE.DISCARD_TREASURE)
