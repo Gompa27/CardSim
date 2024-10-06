e1f1b5aea0e864e3106e351eb85ba317edfee93d
Federico Puente <30831720+Hashiyama89@users.noreply.github.com>
2024-10-06 12:18:00 -0400

    Update sit_and_player.gd

--- sit_and_player.gd
+++ sit_and_player.gd
@@ -2,8 +2,13 @@ extends Control
 
 @export var playerPosition: int
 
+func _ready():
+	if playerPosition == 0:
+		%SitHere.hide()
 
 func _process(delta):
+	%SitHere.disabled = playerPosition >= Game.playersPositions.size()
+	
 	if playerPosition < Game.playersPositions.size():
 		%PlayerName.text = Game.players[Game.playersPositions[playerPosition]].username
 	else:
