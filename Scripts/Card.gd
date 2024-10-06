6e1e86b74956e373ede1a3d82b7dfd477cadbf5f
Federico Puente <30831720+Hashiyama89@users.noreply.github.com>
2024-10-06 12:34:26 -0400

    Actualiza CardPile y girado de las cartas

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -13,15 +13,16 @@ var cardType: Util.CARD_TYPE
 func _ready():
 	cards_in_game.append(self)
 	
-func _process(delta):
+func _process(_delta):
 	%Card.frame = 0 if isFaceDown else cardNumber
 		
 	if isCardSelected:
-		self.global_position = get_global_mouse_position() - offsetMouse
+		self.global_position = get_global_mouse_position() -  _fixOffsetWhenRotate()
 		NetworkManager.move_card(self)
 
 
 func _on_gui_input(event: InputEvent) -> void:
+	print('EVENT: ', event)
 	#El evento es de mouse
 	if event is InputEventMouseButton:
 		#Es click direcho
@@ -45,14 +46,14 @@ func _startDragging(event: InputEventMouseButton):
 	currentCardSelected = self
 
 func _finishDragging():
-	isCardSelected = false
-	offsetMouse = Vector2(0,0)
 	currentCardSelected = null
 	var piles_nodes = Pile.piles
 	var isReparented = false
 	for pile in piles_nodes:
 		if is_dragging_over(pile):
 			self.reparent(pile)
+			self.global_position = get_global_mouse_position() - _fixOffsetWhenRotate()
+
 			isReparented = true
 			NetworkManager.reparent_card(self)
 			pass
@@ -62,6 +63,8 @@ func _finishDragging():
 		self.isFaceDown = true
 		self.reparent(tableNode)
 		NetworkManager.reparent_card(self)
+	isCardSelected = false
+	offsetMouse = Vector2(0,0)
 
 
 func is_dragging_over(pile: Pile) -> bool:
@@ -70,15 +73,29 @@ func is_dragging_over(pile: Pile) -> bool:
 	return drop_area_rect.intersects(pile_rect)
 
 
-
-static func findCard(cardNumber: int, cardType: Util.CARD_TYPE):
+static func findCard(_cardNumber: int, _cardType: Util.CARD_TYPE):
 	var cards = Card.cards_in_game
 	for card in cards:
-		if card.cardNumber == cardNumber && card.cardType == cardType:
+		if card.cardNumber == _cardNumber && card.cardType == _cardType:
 			return card
 
-func moveCard(position: Vector2):
-	self.global_position = position
+func moveCard(_position: Vector2):
+	self.global_position = _position
 	
 func flipCard():
 	isFaceDown = !isFaceDown
+
+
+
+func _fixOffsetWhenRotate():
+	var dX = offsetMouse.x
+	var dY = offsetMouse.y
+	var rotation = self.get_global_transform().get_rotation()
+	if rotation > 3: ### 3.14 === 180
+		return Vector2(-dX, -dY)
+	elif rotation > 1: ### 1.57 === 90
+		return Vector2(-dY, dX)
+	elif rotation < -1 :  ### -1.57 === 270
+		return Vector2(dY, -dX)
+	else:
+		return offsetMouse
