c29ef5fc4b8bdba1f7556f2dcf0f69a194f8705a
Federico Puente <30831720+Hashiyama89@users.noreply.github.com>
2024-10-09 17:31:24 -0400

    Añade colore de jugadores y dado 3d

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -2,12 +2,15 @@ extends Control
 
 var level = 1
 @export var playerNumber: int 
-
+@export var bgcolor: Color
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	%Label.text = str(level)
 	Game.connect('_on_change_level', change_level)
-
+	
+	var styleBox =  StyleBoxFlat.new() 
+	styleBox.bg_color = bgcolor
+	%Label.add_theme_stylebox_override("panel", styleBox)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
--- Scripts/Dice.gd
+++ Scripts/Dice.gd
@@ -9,7 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if isPlaying :
-		get_child(0).frame = randi_range(0,5)
+		for child in get_children():
+			child.frame = randi_range(0,5)
 
 func _on_pressed() -> void:
 	NetworkManager.roll_dice()
@@ -19,5 +20,14 @@ func roll(number : int):
 	await get_tree().create_timer(3).timeout
 	isPlaying = false
 	get_child(0).frame = number
+	get_child(1).frame = _throw_dice_no_repeat([number])
+	get_child(2).frame = _throw_dice_no_repeat([number, get_child(1).frame])
 	
+	
+func _throw_dice_no_repeat(noRepeatNumber: Array[int]):
+	var numberSelected = noRepeatNumber[0]
+	while (numberSelected in noRepeatNumber):
+		numberSelected = randi_range(0,5)
+	
+	return numberSelected
 	
